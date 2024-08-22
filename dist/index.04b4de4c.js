1c95c5c2df4ab0f2657331e2a74d5ca2f0b11dbc
gabrieli <gabrielmolodini2@gmail.com>
2024-08-22 13:29:59 +0400

    1.0.1

---
+++ dist/index.04b4de4c.js
@@ -0,0 +1,2 @@
+console.log(document.getElementById("h1"));
+//# sourceMappingURL=index.04b4de4c.js.map
---
+++ dist/index.44983732.js
@@ -0,0 +1,6 @@
+const h1 = document.getElementById("h1");
+let name;
+console.log(h1) // ge ge
+;
+
+//# sourceMappingURL=index.44983732.js.map
